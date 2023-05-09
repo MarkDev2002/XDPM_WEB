6d2a912c90c0ebd6b94a20a5d350cc9b9a8e64fd
NVTines <ngovantin280702@gmail.com>
2023-05-09 19:40:37 +0700

    .

--- QuanLyBanHang/QuanLyBanHang/Models/Customer/CustomerDeleteViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace QuanLyBanHang.Models.Customer
-{
-    public class CustomerDeleteViewModel
-    {
-        public string nameOfUser { get; set; }
-        public string fullName { get; set; }
-    }
-}
